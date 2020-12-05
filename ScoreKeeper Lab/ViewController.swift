9b563772ae6430312a0a18f1466ae7cf0db7cf87
sunh5 <sunh5@rose-hulman.edu>
2020-12-05 01:25:51 -0500

    Initial Commit

---
+++ ScoreKeeper Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreKeeper Lab
+//
+//  Created by Haoxuan Sun on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
